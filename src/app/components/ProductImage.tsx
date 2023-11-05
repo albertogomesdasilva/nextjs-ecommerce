bb5de13ff3ca39fbcf471747d5238c31c22b194a
Alberto Gomes da Silva <albertogomesdasilva@hotmail.com>
2023-11-04 23:13:37 -0300

    exibindo imagens

---
+++ src/app/components/ProductImage.tsx
@@ -0,0 +1,26 @@
+'use client'
+import { useState } from "react";
+import Image from "next/image";
+import { ProductType } from "@/types/ProductType";
+
+
+type ProductImageProps = {
+    product: ProductType;
+    fill?: boolean;
+    };
+
+export default function ProductImage({ product, fill}: ProductImageProps) {
+    const [loading, setLoading] = useState(true);
+    return fill ? (
+        <Image src={product.image} fill alt="{product.title}" className={`object-cover ${loading ? 'scale-3xl grayscale' 
+        : 'scale-100 blur-0 grayscale-0'         
+    }`} />
+    ) : (
+        <Image src={product.image} 
+        width={400}
+        height={700}
+        fill alt="{product.title}" className={`object-cover ${loading ? 'scale-3xl grayscale' 
+        : 'scale-100 blur-0 grayscale-0'         
+    }`} />
+    );
+}
